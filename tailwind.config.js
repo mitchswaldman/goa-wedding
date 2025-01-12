4280194e9a76683f73163e6be8ff2dc5f125c39d
Mitchell Waldman <mitchswaldman@gmail.com>
2025-01-12 18:05:26 -0500

    Add attire page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', './index.html'],
+  content: [
+    './src/**/*.{html,js}',
+    './{index,attire,travel,faq,events,venue}.html',
+  ],
   theme: {
     extend: {
       colors: {
