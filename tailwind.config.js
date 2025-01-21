23163863c73f9645a2fe3df3690886d334400eb5
Mitchell Waldman <mitchswaldman@gmail.com>
2025-01-20 20:26:10 -0500

    More adjustments. Make card not full width on bigger screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         // rgb(203 0 0)
         'india-red': 'rgb(195 0 0)',
         'india-yellow': '#ffcc00',
-        'india-blue': 'rgb(44 100 206)',
+        'india-blue': 'rgb(172 209 217)',
         'india-green': 'rgb(59 120 82)',
         'india-black': 'rgb(72 71 71)',
         'india-orange': 'rgb(230, 153, 42)',
